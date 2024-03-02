ca6fb242a19098da2b70423f57665162b559baea
Angelo Ceccato <angeloceccato@IT-mac-cean-669341865-old.local>
2024-03-02 19:07:18 +0100

    add script to deploy gh-pages context map demo

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 import jison from './.vite/jisonPlugin.js';
 
 export default defineConfig({
+  base: '',
   plugins: [ jison(), ],
   test: {
     globals: true,
